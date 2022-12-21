a6ce2188de4ccbef959e9b46d4e91cf88c18311b
adrian.siewiera <adrian.siewiera@aptitudesoftware.com>
2022-12-21 16:51:02 +0100

    add kafka

--- src/main/java/gpstracker/HelloController.java
+++ src/main/java/gpstracker/HelloController.java
@@ -1,8 +1,11 @@
 package gpstracker;
 
+import gpstracker.kafka.ProductClient;
+import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Produces;
 
 @Controller("/hello")
@@ -12,6 +15,14 @@ public class HelloController {
     @Produces(MediaType.TEXT_PLAIN)
     public String getHelloWorld() {
         return "Hello World";
+    }
+
+    @Post
+    public String sendToKafka() {
+        ApplicationContext context = ApplicationContext.run();
+        ProductClient client = context.getBean(ProductClient.class);
+        client.sendProduct("Nike", "Blue Trainers");
+        return "xD";
     }
 
 }
---
+++ src/main/java/gpstracker/kafka/ProductController.java
@@ -0,0 +1,21 @@
+package gpstracker.kafka;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import jakarta.inject.Inject;
+
+@Controller("/kafka")
+public class ProductController {
+
+    @Inject
+    private ProductClient productClient;
+
+    @Post("/product")
+    public HttpResponse sendProduct(@Body String name) {
+        productClient.sendProduct("Nike", name);
+        return HttpResponse.ok();
+    }
+
+}
